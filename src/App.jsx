ce0fb47d833e2e39453318fcadcaaeb5c032d504
burgosuc <jlb1983@gmail.com>
2023-11-03 21:14:44 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
+import Context from "./context/MyContext";
+import Favoritas from "./views/Favoritos";
+import Home from "./views/Home";
+
+const PHOTO_URL = "/photos.json";
+
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  const getPhotos = async () => {
+    try {
+      const response = await fetch(PHOTO_URL);
+      if (!response.ok) {
+        throw new Error("Failed to fetch photos.");
+      }
+      const data = await response.json();
+      const transformedPhotos = data.photos.map((photo) => ({
+        id: photo.id,
+        src: photo.src.tiny,
+        alt: photo.alt,
+        liked: false,
+      }));
+      setPhotos(transformedPhotos);
+    } catch (error) {
+      console.error("Error fetching photos:", error);
+    }
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  return (
+    <>
+      <Context.Provider value={{ photos, savePhotos: setPhotos }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<Favoritas />} />
+        </Routes>
+      </Context.Provider>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../assets/css/footer.css";
+
+const Footer = ()=> {
+    return (
+      <p className="footer">Desafío: Natural Pic - Context API. Elaborado por Andry Viloria. Desafío Latam - 2022.</p>
+    );
+  }
+  
+  export default Footer
